8876716cce0e9a25e408f026ebad1e2662976f3c
HoJ0 <31967286+HoJ0@users.noreply.github.com>
2018-05-22 12:39:42 -0400

    Update Task.java

--- app/models/Task.java
+++ app/models/Task.java
@@ -1,1 +1,32 @@
+package models;
+
+import io.ebean.Finder;
+import io.ebean.Model;
+import play.data.validation.Constraints;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+public class Task extends Model {
+
+    public String user;
+
+    public String action;
+
+    public String object;
+
+    public Task(String user, String action, String object){
+
+        this.user = user;
+        this.action = action;
+        this.object = object;
+
+    }
+    
+    public static String getUser(String user) {
+        String firstName = user.substring(0, 1).toUpperCase() + user.substring(1, user.indexOf("."));
+        String lastName = user.substring(user.indexOf(".") + 1, user.indexOf(".") + 2).toUpperCase() + user.substring(user.indexOf(".") + 2, user.indexOf("@") - 2);
+        return firstName + " " + lastName;
+    }
+}
 
